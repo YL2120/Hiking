b0af126bfdcd553320ba92c897946bb524a5dfb1
Yassine Layachi <yassinelayachi@outlook.com>
2021-11-18 12:40:05 +0100

    check input data

--- Hiking.Data/Models/Hike.cs
+++ Hiking.Data/Models/Hike.cs
@@ -20,15 +20,16 @@ namespace Hiking.Data.Models
         public string Difficulty { get; set; }
 
         [Required (AllowEmptyStrings = false)]
-       
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Please enter a positive number with maximum two decimals.")]
         public decimal Distance { get; set; }
 
         [Required(AllowEmptyStrings = false)]
-        [RegularExpression(@"^(?:[01][0-9]|2[0-3]):[0-5][0-9]$", ErrorMessage = "Invalid time format and hh:mm values.")]
+        [RegularExpression(@"((([0-1][0-9])|(2[0-3]))(:[0-5][0-9])(:[0-5][0-9])?)", ErrorMessage = "Time must be between 00:00 to 23:59")]
         [DisplayFormat(DataFormatString =  @"{0:hh\hmm}")]
         public TimeSpan Duration { get; set; }
 
         [Required (AllowEmptyStrings = false, ErrorMessage = "The Height Difference is required")]
+        [RegularExpression(@"^[1-9]+[0-9]*$", ErrorMessage = "Please enter a positive number with no decimals.")]
         public int Height_difference { get; set; }
 
         [Required]
